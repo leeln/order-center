25539be86994c3eaa4054c96200fcea4975d7876
liuzhenyuan <liuzhenyuan@meituan.com>
2016-08-17 15:16:40 +0800

    fix

--- src/test/java/org/micoa/ordercenter/DemoApplicationTests.java
+++ src/test/java/org/micoa/ordercenter/DemoApplicationTests.java
@@ -2,17 +2,25 @@ package org.micoa.ordercenter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OrderCenterApplication.class)
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
