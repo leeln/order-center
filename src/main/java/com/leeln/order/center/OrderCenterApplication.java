bcd87f0265cf86acd0b6021691ef70ba04636d32
liuzhenyuan <liuzhenyuan@meituan.com>
2016-10-19 15:27:56 +0800

    fix

--- src/main/java/com/leeln/order/center/OrderCenterApplication.java
+++ src/main/java/com/leeln/order/center/OrderCenterApplication.java
@@ -2,9 +2,10 @@ package com.leeln.order.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 public class OrderCenterApplication {
 
 	public static void main(String[] args) {
