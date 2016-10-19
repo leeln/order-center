6dfbc3103c5178a3c2402c6d4375912e0bce2586
liuzhenyuan <liuzhenyuan@meituan.com>
2016-10-19 15:27:34 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,6 @@
-FROM leeln/java:jre-8
+FROM docker.repo.leeln.com/leeln/java:jre-8
 
-ENV PROFILES default
-ENV VERSION 0.0.1-SNAPSHOT
-
-ENV HOSTNAME localhost
-ENV SERVER_NAME order-center
-ENV PORT 8080
-
-ENV CONFIG_SERVER_ENABLED false
-ENV CONFIG_SERVER_URL http://config.leeln.com
-
-ENV EUREKA_SERVER_ENABLED true
-ENV EUREKA_SERVER_URL http://eureka:8761/eureka/
+ENV PROFILES prod
 
 ADD build/libs/${SERVER_NAME}-${VERSION}.jar /opt/app/app.jar
 
---
+++ src/main/java/com/leeln/order/center/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.leeln.order.center.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuzhenyuan
+ * @version Last modified 2016/10/19
+ */
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @Value("${app:local}")
+    String app;
+
+    @RequestMapping
+    public String index() {
+        return app;
+    }
+}
